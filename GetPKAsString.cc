739b287b27fdbfe00ed6d67b01061a0bfef6bb78
sonia-alice <60442799+sonia-alice@users.noreply.github.com>
2020-01-29 14:03:23 -0500

    Update GetPKAsString.cc

--- GetPKAsString.cc
+++ GetPKAsString.cc
@@ -1,3 +1,12 @@
+/*
+* This code is used to generate the PK as a
+* string. This is required in my proposed method
+* to store the PK in the blockchain as string is
+* is only datatype for PK that is supported in 
+* Hyperledger Composer. All the PK are stored in the
+* file stringPK.dat.
+*/
+
 #include <iostream>
 #include <vector> 
 #include <sstream>
@@ -33,40 +42,6 @@ int main(int argc, const char* argv[])
         std::string pt = ss.str();          // is the string to be stored in the BC as the Public Key
         
         wfile<<pt<<std::endl;
-
-        //create the PK from the string stored in BC
-        /*HexDecoder decoder;
-        decoder.Put((byte*)&pt[0], pt.size());
-        decoder.MessageEnd();
-        ECP::Point p;
-        size_t len = decoder.MaxRetrievable();
-        p.identity = false;
-        p.x.Decode(decoder, len/2);
-        p.y.Decode(decoder, len/2);
-        ECDSA<ECP, SHA256>::PublicKey PK;
-        PK.Initialize( ASN1::secp256r1(), p );
-        const ECP::Point& point = PK.GetPublicElement();
-        const Integer& pointx = point.x;
-        const Integer& pointy = point.y;*/
-
-        /*k.Load( fs1 );
-        ECDSA<ECP, SHA256>::Signer signer(k);
-        ECDSA<ECP, SHA256>::Verifier verifier(PK);
-        std::string message = "Do or do not. There is no try.";
-        size_t siglen = signer.MaxSignatureLength();
-        std::string signature(siglen, 0x00);
-        siglen = signer.SignMessage( prng, (const byte*)&message[0], message.size(), (byte*)&signature[0] );
-        signature.resize(siglen);*/
-        
-        /*bool result = verifier.VerifyMessage( (const byte*)&message[0], message.size(), (const byte*)&signature[0], signature.size() );
-        if( !result ) {
-            std::cout << "Failed to verify signature on message#" <<1<< std::endl;
-        } else {
-            std::cout << "All good! For MSG"<< std::endl;
-            const Integer& x = k.GetPrivateExponent();
-            //std::cout << "K: " << std::hex << x << std::endl;
-
-        }*/
     }
     wfile.close();
     return 0;
